a9cf9fedab12f175dc62413f531f909647d22947
chiefkarim <krimotop2016@gmail.com>
2025-12-21 16:56:16 +0100

    feat: add logger

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -9,12 +10,18 @@ func main() {
 	serverMux := http.NewServeMux()
 	server := &http.Server{Addr: ":8080", Handler: serverMux}
 
-	serverMux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("./http"))))
+	serverMux.Handle("/app/", logger(http.StripPrefix("/app/", http.FileServer(http.Dir("./http")))))
 	serverMux.HandleFunc("/healthz", func(response http.ResponseWriter, req *http.Request) {
 		response.Header().Add("Content-Type", "text/plain; charset=utf-8")
 		response.WriteHeader(200)
 		response.Write([]byte("OK"))
 	})
-
 	log.Fatal(server.ListenAndServe())
+}
+
+func logger(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(r.Method + ":" + r.RequestURI)
+		next.ServeHTTP(w, r)
+	})
 }
