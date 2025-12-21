b742ac11a920b68ea94a4950e417cec38cde78d9
chiefkarim <krimotop2016@gmail.com>
2025-12-21 17:29:37 +0100

    feat: show how many static files served since server started

--- main.go
+++ main.go
@@ -4,17 +4,38 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileServerHits atomic.Int32
+}
+
+func (config *apiConfig) middlewareMetricInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		config.fileServerHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
 
 func main() {
 	serverMux := http.NewServeMux()
 	server := &http.Server{Addr: ":8080", Handler: serverMux}
+	config := apiConfig{}
 
-	serverMux.Handle("/app/", logger(http.StripPrefix("/app/", http.FileServer(http.Dir("./http")))))
+	serverMux.Handle("/app/", config.middlewareMetricInc(logger(http.StripPrefix("/app/", http.FileServer(http.Dir("./http"))))))
 	serverMux.HandleFunc("/healthz", func(response http.ResponseWriter, req *http.Request) {
 		response.Header().Add("Content-Type", "text/plain; charset=utf-8")
 		response.WriteHeader(200)
 		response.Write([]byte("OK"))
+	})
+	serverMux.HandleFunc("/metrics", func(response http.ResponseWriter, request *http.Request) {
+		response.WriteHeader(200)
+		fmt.Fprintf(response, "Hits: %d", config.fileServerHits.Load())
+	})
+	serverMux.HandleFunc("/reset", func(response http.ResponseWriter, request *http.Request) {
+		config.fileServerHits.Store(0)
+		response.WriteHeader(200)
 	})
 	log.Fatal(server.ListenAndServe())
 }
