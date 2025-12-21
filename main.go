c88e10f43c39b3b0f4b274644fb38cc367d56d26
chiefkarim <krimotop2016@gmail.com>
2025-12-21 16:20:08 +0100

    feat: add health check endpoint

--- main.go
+++ main.go
@@ -8,6 +8,13 @@ import (
 func main() {
 	serverMux := http.NewServeMux()
 	server := &http.Server{Addr: ":8080", Handler: serverMux}
-	serverMux.Handle("/", http.FileServer(http.Dir("./http")))
+
+	serverMux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("./http"))))
+	serverMux.HandleFunc("/healthz", func(response http.ResponseWriter, req *http.Request) {
+		response.Header().Add("Content-Type", "text/plain; charset=utf-8")
+		response.WriteHeader(200)
+		response.Write([]byte("OK"))
+	})
+
 	log.Fatal(server.ListenAndServe())
 }
